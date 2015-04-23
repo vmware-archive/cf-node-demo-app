c3245d3f6e35dbeb224e6eb6b9a90c47af525722
imorti <imorti@gmail.com>
2015-04-22 21:24:04 -0400

    properly added newrelic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['imorti-node'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '2e271686303f580eb4ce090aea4015bac2b3c1b9',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
